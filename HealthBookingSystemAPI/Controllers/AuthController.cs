b73db54c088d2824948fe06875d15cc958f71d22
Lê Thế Bảo <lethebao1404@gmail.com>
2025-11-15 11:51:06 +0700

    add AuthController, change GetUserByEmailAndPassword => Login, add new css

---
+++ HealthBookingSystemAPI/Controllers/AuthController.cs
@@ -0,0 +1,114 @@
+﻿using BusinessObject.Models;
+using HealthBookingSystemAPI.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Services.Interface;
+using Services.Service;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthBookingSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IPatientService _patientService;
+        public AuthController(IUserService service, IPatientService patientService)
+        {
+            _userService = service;
+            _patientService = patientService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginDTO)
+        {
+            var account = await _userService.Login(loginDTO.Email, loginDTO.Password);
+            if (account == null)
+            {
+                return Unauthorized("Invalid email or password.");
+            }
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, account.Email),
+                    new Claim("Role", account.Role.ToString()),
+                    new Claim("AccountId", account.UserId.ToString()),
+                };
+
+            var symetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+            var signCredential = new SigningCredentials(symetricKey, SecurityAlgorithms.HmacSha256);
+
+            var preparedToken = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(16),
+                signingCredentials: signCredential);
+
+            var generatedToken = new JwtSecurityTokenHandler().WriteToken(preparedToken);
+            var role = account.Role;
+            var accountId = account.UserId;
+
+            return Ok(new AccountResponseDTO
+            {
+                Role = role,
+                Token = generatedToken,
+                AccountId = accountId,
+            });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            var existUser = await _userService.CheckUserExist(registerDTO.Email);
+            if (existUser == null)
+            {
+                var user = new User
+                {
+                    Email = registerDTO.Email,
+                    Password = registerDTO.Password,
+                    FullName = registerDTO.FullName,
+                    PhoneNumber = registerDTO.PhoneNumber,
+                    Role = registerDTO.Role,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    IsActive = true,
+                    AvatarUrl = "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+                };
+
+                await _userService.CreateUser(user);
+
+                if (user.Role.ToLower().Equals("patient"))
+                {
+                    user.Patient = new Patient
+                    {
+                        UserId = user.UserId,
+                        DateOfBirth = registerDTO.Dob,
+                        Gender = registerDTO.Gender,
+                        Address = registerDTO.Address,
+                        BloodType = registerDTO.BloodType,
+                        EmergencyPhoneNumber = registerDTO.EmergencyContact,
+                        Weight = registerDTO.Weight,
+                        Height = registerDTO.Height,
+                        Bmi = registerDTO.Bmi,
+                        Allergies = registerDTO.Allergies,
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+
+                    _patientService.AddPatient(user.Patient);
+                }
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ HealthBookingSystemAPI/Controllers/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interface;
+
+namespace HealthBookingSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientService _patientService;
+        public PatientsController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+        [HttpGet("doctor/{id}")]
+        public async Task<IActionResult> GetPatientsByDoctorId(int id)
+        {
+            var patients = await _patientService.GetPatientsByDoctorAsync(id);
+            return Ok(patients);
+        }
+        [HttpGet("doctor/new/{id}")]
+        public async Task<IActionResult> GetNewPatientsByDoctorId(int id)
+        {
+            var patients = await _patientService.GetNewPatientsAsync(id);
+            return Ok(patients);
+        }
+    }
+}
